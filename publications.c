4c2345145dc21b069e7263489bce770f2176ec57
raduqq <radustefan.minea@yahoo.com>
2020-05-04 21:01:35 +0300

    Added first files

---
+++ publications.c
@@ -0,0 +1,92 @@
+#include "publications.h"
+
+struct publications_data {
+    /* TODO: add whichever data you need here */
+};
+
+PublData* init_publ_data(void) {
+    /* TODO: implement init_publ_data */
+    return NULL;
+}
+
+void destroy_publ_data(PublData* data) {
+    /* TODO: implement destroy_publ_data */
+}
+
+void add_paper(PublData* data, const char* title, const char* venue,
+    const int year, const char** author_names, const int64_t* author_ids,
+    const char** institutions, const int num_authors, const char** fields,
+    const int num_fields, const int64_t id, const int64_t* references,
+    const int num_refs) {
+    /* TODO: implement add_paper */
+}
+
+char* get_oldest_influence(PublData* data, const int64_t id_paper) {
+    /* TODO: implement get_oldest_influence */
+
+    return NULL;
+}
+
+float get_venue_impact_factor(PublData* data, const char* venue) {
+    /* TODO: implement get_venue_impact_factor */
+
+    return 0.f;
+}
+
+int get_number_of_influenced_papers(PublData* data, const int64_t id_paper,
+    const int distance) {
+    /* TODO: implement get_number_of_influenced_papers */
+
+    return -1;
+}
+
+int get_erdos_distance(PublData* data, const int64_t id1, const int64_t id2) {
+    /* TODO: implement get_erdos_distance */
+
+    return -1;
+}
+
+char** get_most_cited_papers_by_field(PublData* data, const char* field,
+    int* num_papers) {
+    /* TODO: implement get_most_cited_papers_by_field */
+
+    return NULL;
+}
+
+int get_number_of_papers_between_dates(PublData* data, const int early_date,
+    const int late_date) {
+    /* TODO: implement get_number_of_papers_between_dates */
+
+    return 0;
+}
+
+int get_number_of_authors_with_field(PublData* data, const char* institution,
+    const char* field) {
+    /* TODO: implement get_number_of_authors_with_field */
+
+    return 0;
+}
+
+int* get_histogram_of_citations(PublData* data, const int64_t id_author,
+    int* num_years) {
+    /* TODO: implement get_histogram_of_citations */
+
+    *num_years = 0;
+
+    return NULL;
+}
+
+char** get_reading_order(PublData* data, const int64_t id_paper,
+    const int distance, int* num_papers) {
+    /* TODO: implement get_reading_order */
+
+    *num_papers = 0;
+
+    return NULL;
+}
+
+char* find_best_coordinator(PublData* data, const int64_t id_author) {
+    /* TODO: implement find_best_coordinator */
+
+    return NULL;
+}
