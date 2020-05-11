b3958fffee61d18f70392d0667a425fca4a8816b
raduqq <radustefan.minea@yahoo.com>
2020-05-11 18:30:19 +0300

    Introduced HT. Needs refining

--- publications.c
+++ publications.c
@@ -8,6 +8,7 @@
 #define MAX_LEN 200
 #define MAX_AUTHORS 100
 #define MAX_FIELDS 50
+#define HMAX 112000
 
 struct author {
     char *name;
@@ -27,6 +28,55 @@ struct publications_data {
     int64_t *references;
     int num_refs;
 };
+
+typedef struct Hashtable {
+    PublData *buckets;
+    int hmax;
+    unsigned int (*hash_function)(void*);
+    int (*compare_function)(void *, void *);
+} Hashtable;
+
+unsigned int hash_function_int(void *a) {
+    /*
+     * Credits: https://stackoverflow.com/a/12996028/7883884
+     */
+    unsigned int uint_a = *((unsigned int *)a);
+
+    uint_a = ((uint_a >> 16u) ^ uint_a) * 0x45d9f3b;
+    uint_a = ((uint_a >> 16u) ^ uint_a) * 0x45d9f3b;
+    uint_a = (uint_a >> 16u) ^ uint_a;
+    return uint_a;
+}
+
+int compare_function_ints(void *a, void *b) {
+    int int_a = *((int *)a);
+    int int_b = *((int *)b);
+
+    if (int_a == int_b) {
+        return 0;
+    } else if (int_a < int_b) {
+        return -1;
+    } else {
+        return 1;
+    }
+}
+
+void init_ht(struct Hashtable *ht, int hmax, unsigned int (*hash_function)(void*), int (*compare_function)(void*, void*)) {
+    ht->buckets = (PublData*) malloc(hmax * sizeof(PublData));
+
+    if (ht->buckets == NULL) {
+        fprintf(stderr, "No buckets found!\n");
+        exit(-1);
+    }
+
+    for (int i = 0; i < hmax; i++) {
+        ht->buckets[i] = malloc(sizeof(PublData));
+    }
+
+    ht->hmax = hmax;
+    ht->hash_function = hash_function;
+    ht->compare_function = compare_function;
+}
 
 PublData* init_publ_data(void) {
     PublData *data = malloc(sizeof(PublData));
@@ -99,12 +149,23 @@ void destroy_publ_data(PublData* data) {
     free(data->references);
 }
 
+void destroy_hashtable(Hashtable *ht) {
+    for (int i = 0; i < ht->hmax; i++) {
+        free(&ht->buckets[i]);
+    }
+    
+    free(ht);
+}
+
 void add_paper(PublData* data, const char* title, const char* venue,
     const int year, const char** author_names, const int64_t* author_ids,
     const char** institutions, const int num_authors, const char** fields,
     const int num_fields, const int64_t id, const int64_t* references,
     const int num_refs) {
 
+    int hash = ht->
+        
+    // Put-ul
     data->title = title;
     data->venue = venue;
     data->year = year;
